6f6a3847186437bf7c9b73977dcb6ff34b5beaa0
Max0607 <42951680+Max0607@users.noreply.github.com>
2019-07-29 20:43:35 -0500

    Add files via upload

---
+++ Desafio04_01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio04_01
+{
+    class Desafio04_01
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su salario: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (0 <= salario && salario <= 1656232)
+            {
+                Console.WriteLine("Usted pertenece a la tarifa A");   
+            }
+            else if (1656232  <= salario && salario <= 3312464)
+            {
+                Console.WriteLine("Usted pertenece a la tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Usted pertenece a la tarifa C");
+            }
+        }
+    }
+}
---
+++ Desafio04_02.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio04_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hace usted parte del regimen subsidiado (SISBEN)?: ");
+            string Sisben = Console.ReadLine();
+
+            if(Sisben.ToUpper() == "SI")
+            {
+                Console.WriteLine("Al ser parte del regimen subsidiado usted no paga cuota moderadora");
+            }
+            else
+            {
+                Console.Write("Ingrese su salario: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                if (0 <= salario && salario <= 1656231 )
+                {
+                    Console.WriteLine("Usted debe pagar una cuota moderadora de 3.200 pesos (Tarifa A)");
+                }
+                else if (1656231 <= salario && salario <= 4140580)
+                {
+                    Console.WriteLine("Usted debe pagar una cuota moderadora de 12.700 pesos (Tarifa B)");
+                }
+                else 
+                {
+                    Console.WriteLine("Usted debe pagar una cuota moderadora de 33.500 pesos (Tarifa C)");
+                }
+            }
+        }
+
+    }
+}
